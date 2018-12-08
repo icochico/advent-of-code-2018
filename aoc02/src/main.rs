b82b84eb62e05d1903341b3b2d5aef936cb97196
Enrico <enrico@aira.io>
2018-12-07 23:32:24 -0800

    Added day2 part2

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -13,6 +13,7 @@ fn main() {
     println!("main: size of text is {} bytes", size);
 
     part1(&contents);
+    part2(&contents);
 }
 
 fn part1(input: &str) {
@@ -34,7 +35,7 @@ fn part1(input: &str) {
             }
         }
 
-        for (_ , v) in seen.iter() {
+        for (_, v) in seen.iter() {
             match *v
                 {
                     2 => has_double = true,
@@ -50,4 +51,37 @@ fn part1(input: &str) {
     println!("Found doubles: {}", doubles);
     println!("Found triples: {}", triples);
     println!("Checksum: {}", doubles * triples)
+}
+
+fn part2(input: &str) {
+    let ids: Vec<&str> = input.lines().collect();
+    for i in 0..ids.len() {
+        for j in i + 1..ids.len() {
+            if let Some(common) = common_correct_letters(&ids[i], &ids[j]) {
+                println!("Common correct letters: {}", common);
+            }
+        }
+    }
+}
+
+fn common_correct_letters(id1: &str, id2: &str) -> Option<String> {
+    if id1.len() != id2.len() {
+        return None;
+    }
+
+    let mut found_one_wrong = false;
+    for (c1, c2) in id1.chars().zip(id2.chars()) {
+        if c1 != c2 {
+            if found_one_wrong {
+                return None;
+            }
+            found_one_wrong = true;
+        }
+    }
+    Some(
+        id1.chars().zip(id2.chars())
+            .filter(|&(c1, c2)| c1 == c2)
+            .map(|(c, _)| c)
+            .collect()
+    )
 }
